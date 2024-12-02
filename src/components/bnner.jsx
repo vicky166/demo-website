138cc4e46583e3ef64df98bd3b5d5bccc9f72c71
vicky166 <vickyempire723@gmail.com>
2024-12-02 15:56:40 +0530

    making  improvments

--- src/components/bnner.jsx
+++ src/components/bnner.jsx
@@ -1,6 +1,6 @@
 export const Banner = () => {
   return (
-    <div className="w-full bg-violet-900 rounded-t-[20px] px-4 sm:px-6 lg:px-8">
+    <div className="w-full bg-violet-900  px-4 sm:px-6 lg:px-8">
       <p className="text-white text-center py-2 text-xs sm:text-sm md:text-base">
         This is an announcement tagline for you to insert
       </p>
